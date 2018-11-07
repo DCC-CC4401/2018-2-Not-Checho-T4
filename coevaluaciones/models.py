15685ee8873045341b52c1df9ed398d17c60b7b0
Valeria Guidotti <valeriaguidotti@gmail.com>
2018-11-07 18:50:16 -0300

    Agrega modelos de Roles y Curso

--- coevaluaciones/models.py
+++ coevaluaciones/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=200)
+    codigo = models.CharField(max_length=10)
+    seccion = models.IntegerField(default=1)
+    anho = models.IntegerField()
+    semestre = models.IntegerField()
+
+    class Meta:
+        unique_together = (("codigo", "seccion", "anho", "semestre"),)
+
+
+class Roles(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rol = models.CharField(max_length=50)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
